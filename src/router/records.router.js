699060b3a69525882381b758a15a6c6c691ede74
aaronlyy <aaronlevican@gmail.com>
2023-03-13 10:27:44 +0100

    fixed http method in router

---
+++ src/router/records.router.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+import {
+    controllerRecordsGetAll,
+    controllerRecordsDeleteAll,
+    controllerRecordsDeleteOne,
+    controllerRecordsGetOne,
+    controllerRecordsPatchOne
+} from '../controller/records.controller';
+
+const routerRecords = express.Router();
+
+routerRecords.get('/', controllerRecordsGetAll) // get all records
+routerRecords.delete('/', controllerRecordsDeleteAll) // delete all records
+routerRecords.get('/:rid', controllerRecordsGetOne) // get specific record
+routerRecords.patch('/:rid', controllerRecordsPatchOne) // patch specific record
+routerRecords.delete('/:rid', controllerRecordsDeleteOne) // delete specific record
+
+
+export default routerRecords;
